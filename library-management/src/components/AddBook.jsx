b8ca3faa052729c64e921d7c2781e35377db8cb7
Rashid Ali Khan <rashid.11612612@gmail.com>
2023-11-09 15:38:09 +0530

    remove unused code

--- library-management/src/components/AddBook.jsx
+++ library-management/src/components/AddBook.jsx
@@ -112,7 +112,6 @@ function AddBook()
                  <div className="input-data" style={{float:'right'}}>
                     <div className="inner"></div>
                     <input type="submit" value="submit" />
-                  
                  </div>
               </div>
               </div>
