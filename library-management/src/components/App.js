87700a04f7114f4b08e29a371c14715ae71749bb
Rashid Ali Khan <rashid.11612612@gmail.com>
2023-11-17 18:10:57 +0530

    library-management using react js

---
+++ library-management/src/components/App.js
@@ -0,0 +1,13 @@
+import '../css/App.css';
+import ResponsiveAppBar from './ResponsiveAppBar';
+// import api from "../server/axiosConfigServer";
+
+
+function App() {
+
+  return (
+        <ResponsiveAppBar />
+  );
+}
+
+export default App;
